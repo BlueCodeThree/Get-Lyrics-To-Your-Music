6679a057d3ecee23f3b22629c7be68ea4d4e5f3d
BlueCodeThree <47844116+BlueCodeThree@users.noreply.github.com>
2019-03-14 12:11:13 +1000

    IT WORKS!

--- lyrics_currently_playing.py
+++ lyrics_currently_playing.py
@@ -11,9 +11,11 @@ import json
 import webbrowser
 import os
 from json.decoder import JSONDecodeError
+from musixmatch import Musixmatch
 
 username = apikey.spotify_username
 scope = 'user-read-currently-playing'
+musixmatch = Musixmatch(apikey.musixmatch_api)
 
 # getting a token
 try:
@@ -27,9 +29,21 @@ except:
 # spotify object
 sp = spotipy.Spotify(auth=token)
 
+# Gets the currently playing artist and song name
 current_playing_artist = sp.current_user_playing_track()['item']['album']['artists'][0]['name']
 print("Artist = " + current_playing_artist)
-
 current_playing_song = sp.current_user_playing_track()['item']['name']
 print("Song = " + current_playing_song)
+
+# search for the track id
+track_id = musixmatch.track_search(current_playing_song, current_playing_artist, page_size=10, page=1, s_track_rating='desc')['message']['body']['track_list'][0]['track']['track_id']
+
+# get the lyrics of a song
+def get_lyrics(track_id):
+    lyrics = musixmatch.track_lyrics_get(track_id)['message']['body']['lyrics']['lyrics_body']
+    return lyrics
+
+# Terminal prints out the track ID and the lyrics
+print("Lyrics: ")
+print(get_lyrics(track_id))
 

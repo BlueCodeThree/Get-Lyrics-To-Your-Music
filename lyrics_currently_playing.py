a8118c23c8af060bdb0a2c1743ce0f2124e63394
BlueCodeThree <47844116+BlueCodeThree@users.noreply.github.com>
2019-03-14 12:04:12 +1000

    grabs song & artist of currently playing

--- lyrics_currently_playing.py
+++ lyrics_currently_playing.py
@@ -3,9 +3,33 @@
 # https://github.com/BlueCodeThree
 # Grabs the song spotify is currently playing and shows you the lyrics to it. 
 
-import requests
 import spotipy
+import spotipy.util as util
+import apikey
+import sys
+import json
+import webbrowser
+import os
+from json.decoder import JSONDecodeError
 
-sp = spotipy.Spotify()
+username = apikey.spotify_username
+scope = 'user-read-currently-playing'
 
-util.prompt_for_user_token(username,scope,client_id='your-app-redirect-url',client_secret='your-app-redirect-url',redirect_uri='your-app-redirect-url')
+# getting a token
+try:
+    token = util.prompt_for_user_token(username,scope,client_id=apikey.spotify_id,client_secret=apikey.spotify_secret,redirect_uri=apikey.spotify_redirect)
+except:
+    print("Can't get token for", username)
+    os.remove(f".cache-{username}")
+    token = token = util.prompt_for_user_token(username,scope,client_id=apikey.spotify_id,client_secret=apikey.spotify_secret,redirect_uri=apikey.spotify_redirect)
+
+
+# spotify object
+sp = spotipy.Spotify(auth=token)
+
+current_playing_artist = sp.current_user_playing_track()['item']['album']['artists'][0]['name']
+print("Artist = " + current_playing_artist)
+
+current_playing_song = sp.current_user_playing_track()['item']['name']
+print("Song = " + current_playing_song)
+
